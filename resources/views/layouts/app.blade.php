4554c853720148120f2226de8f05f73ff675ed65
Roberto Scarci <145481687+RobertoScarci@users.noreply.github.com>
2024-02-02 13:48:46 +0100

    Initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    @vite('resources/js/app.js')
+    
+</head>
+<body>
+    @include('layouts.partials.header')
+
+        <main>
+            @yield('main-content')
+        </main>
+
+    @include('layouts.partials.footer')
+</body>
+</html>
